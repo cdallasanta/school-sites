ca0ddc335f5b245f897a75234696045670d6f0c4
cdallasanta <chris.dallasanta@gmail.com>
2019-09-02 13:22:47 -0700

    setting up testing SchoolSelector

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SchoolSelector from './components/SchoolSelector';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    district: null
+  }
+
+  componentDidMount(){
+    fetch("http://localhost:3001/api/districts/1")
+      .then(resp => {
+        debugger;
+        this.setstate({district: resp})
+      })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <SchoolSelector district={this.props.district} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- client/src/components/SchoolSelector.js
+++ client/src/components/SchoolSelector.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class SchoolSelector extends React.Component {
+  state = {
+    school_id: null
+  }
+
+  handleChange = e => {
+    this.setState({school_id: e.target.value})
+  }
+
+  optionsForSelect = () => {
+    return this.props.district.schools.map(school => {
+      return <option value={school.id} >{school.name}</option>
+    })
+  }
+
   render(){
+    let resp;
+    if (this.state.district){
+      resp = <select value={this.state.value} onChange={this.handleChange}>
+          {this.optionsForSelect()}
+        </select>
+    } else {
+      resp = "Loading schools"
+    }
+    
     return (
       <div>
-        
+        {resp}
       </div>
     )
   }
