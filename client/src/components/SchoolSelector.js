c8eda56c7ede4da5e16f7333eb3275dfd8aa40b2
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-02 12:53:25 -0700

    added blank containers and components, started on SchoolSelector

---
+++ client/src/components/SchoolSelector.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SchoolSelector extends React.Component {
+  render(){
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default SchoolSelector;
