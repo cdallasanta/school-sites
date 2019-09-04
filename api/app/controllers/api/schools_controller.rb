7c210ae37cd511bc42a48d9d235be7ffe10e9642
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-04 09:47:50 -0700

    added schools index route to speed up selector

--- api/app/controllers/api/schools_controller.rb
+++ api/app/controllers/api/schools_controller.rb
@@ -1,4 +1,8 @@
 class API::SchoolsController < ApplicationController
+  def index
+    render json: School.all.to_json(only: [:id, :name])
+  end
+
   def show
     render json: School.find(params[:id])
   end
