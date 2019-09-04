bdfe52bb8b243b68370ed1172e091c1f29169d3b
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-04 15:44:02 -0700

    fixed events serializer

--- api/app/serializers/event_serializer.rb
+++ api/app/serializers/event_serializer.rb
@@ -1,5 +1,5 @@
 class EventSerializer < ActiveModel::Serializer
-  attributes :id, :title, :date, :description
+  attributes :id, :title, :start, :end, :description
 
   belongs_to :user, class_name: "User", foreign_key: :organizer_id, optional: true
   belongs_to :eventable, polymorphic: true
