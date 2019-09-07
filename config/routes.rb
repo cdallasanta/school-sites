fd7628213a6c1e920281b07cee3c7ca42697d0ae
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-07 13:19:21 -0700

    added root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   default_url_options :host => "localhost:3001"
+
+  root :to => redirect('/districts/1')
+
   namespace :api do
     resources :users do 
       get :avatar, on: :member
