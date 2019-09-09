38aa42d64beff84c3b8a2f628ef29b76fe48fcba
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-09 14:11:13 -0700

    npm installed

---
+++ client/build/precache-manifest.f38106274d08952f8d6f7d6507529567.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9500a34d927107ec45b2a4433b577a2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c567c13dbb6df9b33a6",
+    "url": "/static/css/2.48adc591.chunk.css"
+  },
+  {
+    "revision": "8af3b2548d832e9502e8",
+    "url": "/static/css/main.fce93770.chunk.css"
+  },
+  {
+    "revision": "1c567c13dbb6df9b33a6",
+    "url": "/static/js/2.cf8ba48e.chunk.js"
+  },
+  {
+    "revision": "8af3b2548d832e9502e8",
+    "url": "/static/js/main.51276733.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  }
+]);
