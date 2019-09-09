dfdad6b16f93ee431ff9e9a916ae40e7e0a7b42a
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-09 09:18:26 -0700

    added 404 component to test heroku and routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import SchoolSelector from './components/SchoolSelector';
 import SchoolContainer from './containers/SchoolContainer';
 import DistrictContainer from './containers/DistrictContainer';
 import CalendarCont from './oldCalendar/Calendar';
+import NotFound from './components/NotFound';
 
 class App extends React.Component {
   render(){
     return (
       <div className="App">
         <BrowserRouter>
-          <Route exact path="/" component={SchoolSelector} />
-          <Route exact path="/districts/:dist_id/selector" component={SchoolSelector} />
-          <Route exact path="/districts/:dist_id/schools/:school_id" component={SchoolContainer} />
-          <Route exact path="/districts/:dist_id" component={DistrictContainer} />
-          <Route exact path="/districts/:dist_id/calendar" component={CalendarCont} />
+          <Switch>
+            <Route exact path="/" component={SchoolSelector} />
+            <Route exact path="/districts/:dist_id/selector" component={SchoolSelector} />
+            <Route exact path="/districts/:dist_id/schools/:school_id" component={SchoolContainer} />
+            <Route exact path="/districts/:dist_id" component={DistrictContainer} />
+            <Route exact path="/districts/:dist_id/calendar" component={CalendarCont} />
+            
+            <Route component={NotFound} />
+          </Switch>
         </BrowserRouter>
       </div>
     );
---
+++ client/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class NotFound extends Component {
+  render () {
+    return <div textAlign='center'>
+      <h1>404: Not found</h1>
+    </div>
+  }
+}
+
+export default NotFound
