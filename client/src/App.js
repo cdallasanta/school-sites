dd25f03e66f21aee36e402bdc262f58c1d4a40ae
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-11 16:25:37 -0700

    continuing to set up devise_auth_token

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SchoolSelector from './components/SchoolSelector';
 import SchoolContainer from './containers/SchoolContainer';
 import DistrictContainer from './containers/DistrictContainer';
 import CalendarCont from './oldCalendar/Calendar';
+import Login from './components/Login';
 
 class App extends React.Component {
   render(){
@@ -16,6 +17,7 @@ class App extends React.Component {
             <Route exact path="/districts/:dist_id/schools/:school_id" component={SchoolContainer} />
             <Route exact path="/districts/:dist_id" component={DistrictContainer} />
             <Route exact path="/districts/:dist_id/calendar" component={CalendarCont} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </BrowserRouter>
       </div>
---
+++ client/src/components/Login.js
@@ -0,0 +1,50 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+  state = {
+    email: "",
+    password: ""
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault();
+
+    fetch({
+      type: 'POST',
+      url: 'http://localhost:3000/auth/sign_in',
+      data: {
+        email: this.state.email,
+        password: this.state.password
+      }
+    })
+    .then((response, status, jqXHR) => {
+      sessionStorage.setItem('user',
+        JSON.stringify({
+          'access-token': jqXHR.getResponseHeader('access-token'),
+          client: jqXHR.getResponseHeader('client'),
+          uid: response.data.uid
+        })
+      );
+      this.props.history.push('/');
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Sign in</h2>
+        <form onSubmit={this.handleLogin} >
+          <input name="email" value={this.state.email} onChange={this.handleChange} />
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
