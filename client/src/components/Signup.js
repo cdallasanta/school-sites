8935dea32703d6965086fbba4990a58b6fd6641a
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-09-13 12:42:31 -0700

    added signup component, current has copied code from login

---
+++ client/src/components/Signup.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import $ from 'jquery';
+import '../stylesheets/login.scss';
+
+export default class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    errors: []
+  }
+
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+
+  handleLogin = (e) => {
+    e.preventDefault();
+    $("login-errors").empty();
+
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:3001/auth/sign_in',
+      data: {
+        email: this.state.email,
+        password: this.state.password
+      },
+      success: (response, status, jqXHR) => {
+        sessionStorage.setItem('user',
+          JSON.stringify({
+            'access-token': jqXHR.getResponseHeader('access-token'),
+            client: jqXHR.getResponseHeader('client'),
+            uid: response.data.uid
+          })
+        );
+  // use headers: JSON.parse(sessionStorage.user) in ajax calls to api
+        this.props.history.push('/');
+      },
+      error: resp => {
+        this.setState({errors: resp.responseJSON.errors})
+      }
+    })
+  }
+
+  renderErrors = () => {
+    return (
+      <div id="login-errors" className="errors">
+        {
+          this.state.errors.map((error, i) => {
+            return <li key={i}>{error}</li>
+          })
+        }
+      </div>
+    )
+  }
+
+  render () {
+    return (
+      <div>
+        <h2>Sign in</h2>
+        {this.state.errors.length > 0 ? this.renderErrors() : null}
+        <form onSubmit={this.handleLogin} >
+          <input name="email" value={this.state.email} onChange={this.handleChange} />
+          <input name="password" type="password" value={this.state.password} onChange={this.handleChange} />
+          <input type="submit"/>
+        </form>
+      </div>
+    )
+  }
+}
